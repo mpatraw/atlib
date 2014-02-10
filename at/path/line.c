b4823b7ea997c0b26f8dca1a122a9e4b8106d89c
Michael Patraw <gruebite@gmail.com>
2014-02-09 22:57:11 -0800

    fixed formatting

--- at/path/line.c
+++ at/path/line.c
@@ -7,25 +7,26 @@
 
 void at_path_line(int *path, unsigned *sz, int x0, int y0, int x1, int y1)
 {
-    int dx = abs(x1 - x0);
-    int sx = x0 < x1 ? 1 : -1;
-    int dy = abs(y1 - y0);
-    int sy = y0 < y1 ? 1 : -1;
-    int err = (dx > dy ? dx : -dy) / 2, e2;
-    int i = 0;
+        int dx = abs(x1 - x0);
+        int sx = x0 < x1 ? 1 : -1;
+        int dy = abs(y1 - y0);
+        int sy = y0 < y1 ? 1 : -1;
+        int err = (dx > dy ? dx : -dy) / 2, e2;
+        int i = 0;
 
-    for (;;) {
-        /* todo: should this check be here? */
-        /*if (i >= *sz - 1) break;*/
-        path[i * 2 + 0] = x0;
-        path[i * 2 + 1] = y0;
-        i++;
+        for (;;)
+        {
+                /* todo: should this check be here? */
+                /*if (i >= *sz - 1) break;*/
+                path[i * 2 + 0] = x0;
+                path[i * 2 + 1] = y0;
+                i++;
 
-        if (x0 == x1 && y0 == y1) break;
-        e2 = err;
-        if (e2 > -dx) { err -= dy; x0 += sx; }
-        if (e2 <  dy) { err += dx; y0 += sy; }
-    }
+                if (x0 == x1 && y0 == y1) break;
+                e2 = err;
+                if (e2 > -dx) { err -= dy; x0 += sx; }
+                if (e2 <  dy) { err += dx; y0 += sy; }
+        }
 
-    *sz = i;
+        *sz = i;
 }
